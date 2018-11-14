e5ad47bda64dd490fcaccfac18015e0abb0228f1
chrysn <chrysn@fsfe.org>
2018-11-14 12:44:31 +0100

    Update example to be actually no_std
    This also means that dev dependencies get their features stripped down
    in no_std mode.

--- examples/decode_no_std.rs
+++ examples/decode_no_std.rs
@@ -1,4 +1,16 @@
-#![no_std]
+// To make this actually runnable, remove the comment signs from the below, and add libc to
+// dev-dependencies
+
+// #![feature(start)]
+// #![no_std]
+//
+// extern crate libc;
+//
+// #[start]
+// fn start(_argc: isize, _argv: *const *const u8) -> isize {
+//     main();
+//     0
+// }
 
 #[macro_use]
 extern crate serde_derive;
